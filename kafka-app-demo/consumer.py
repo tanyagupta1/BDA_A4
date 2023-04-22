42b5df2fdf89fccf2249d6ff233c375ef02e24e7
tanyagupta1 <tanya19119@iiitd.ac.in>
2023-04-22 11:37:32 +0530

    basic producer-consumer implemented

---
+++ kafka-app-demo/consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+import json
+
+if __name__=="__main__":
+    consumer = KafkaConsumer(
+        "registered_user",
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest',
+        group_id="consumer-group-a"
+        )
+    print("starting the consumer")
+    for msg in consumer:
+        print("Register User ={}".format(json.loads(msg.value)))
---
+++ kafka-app-demo/producer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+import json
+from data import get_registered_user
+import time
+
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=json_serializer)
+
+if __name__=="__main__":
+    while True:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        time.sleep(4)
